5a3acf70fc5d10b142fddcaca184ada420a33c07
Kwesi Berko <kwesi_berko@Kwesis-MacBook-Pro.local>
2019-06-24 00:12:50 -0400

    Header Changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Heshimagh',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Heshima-header">
+        <div className="Heshima-header-group">
+        <h1>Heshimatours</h1>
+        <p>Car Rental & Tours</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
